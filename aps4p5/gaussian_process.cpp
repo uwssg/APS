58b710d1c0dbd9b3444d11b2c0233d1beae0975c
danielsf <scott.f.daniel@gmail.com>
2014-01-01 14:06:05 -0800

    changed dosrch so that it is based on 0.5*ddmax

--- aps4p5/gaussian_process.cpp
+++ aps4p5/gaussian_process.cpp
@@ -1522,7 +1522,7 @@ int neighbor_cache::compare(double *newpt, int kkin){
     }
     if(kk!=kkin) return 1;
     dist=kptr->distance(pt,newpt);
-    if(dist<dd[kk/2])return 0;
+    if(dist<0.5*dd[kk-1])return 0;
     else return 1;
 }
 
