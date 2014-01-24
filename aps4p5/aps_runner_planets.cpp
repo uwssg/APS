3b326cccecc314abe90b938e60a39197b9cff3a7
danielsf <scott.f.daniel@gmail.com>
2014-01-24 11:52:24 -0800

    planet compiles

---
+++ aps4p5/aps_runner_planets.cpp
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "aps.h"
+#include "exoplanet.h"
+
+main(int iargc, char *argv[]){
+
+int seed=-1;
+int dim;
+
+if(iargc>1){
+    seed=atoi(argv[1]);
+}
+else{
+    seed=int(time(NULL));
+}
+
+Ran chaos(seed);
+
+dim=10;
+
+matern_covariance cv;
+
+planet chisq(5);
+
+aps aps_test(dim,20,33.93,seed);
+
+aps_test.assign_chisquared(&chisq);
+aps_test.assign_covariogram(&cv);
+
+aps_test.set_write_every(10);
+aps_test.set_grat(1.0);
+
+array_1d<double> max,min;
+max.set_name("driver_max");
+min.set_name("driver_min");
+max.set_dim(dim);
+min.set_dim(dim);
+
+min.set(0,4.0);
+max.set(0,6.0);
+
+min.set(1,240.0);
+max.set(1,270.0);
+
+min.set(2,0.0);
+max.set(2,10.0);
+
+min.set(3,30.0);
+max.set(3,50.0);
+
+min.set(4,1.0);
+max.set(4,40.0);
+
+min.set(5,5000.0);
+max.set(5,5300.0);
+
+min.set(6,20.0);
+max.set(6,50.0);
+
+min.set(7,7.0);
+max.set(7,21.0);
+
+min.set(8,-106.0);
+max.set(8,100.0);
+
+min.set(9,0.0);
+max.set(9,10.0);
+
+aps_test.initialize(100,min,max);
+double chival,chivaltest,err,maxerr;
+
+int i;
+i=-1;
+while(aps_test.get_n_pts()<500000){
+    aps_test.search();
+    
+    
+}
+
+
+
+printf("ct_aps %d ct_grad %d total %d\n",
+aps_test.get_ct_aps(),aps_test.get_ct_gradient(),
+aps_test.get_called());
+
+
+printf("maxerr %e npts %d\n",maxerr,aps_test.get_n_pts());
+aps_test.write_pts();
+}
