62992b8d682ed79885b662a9de1d3c356feafc19
danielsf <scott.f.daniel@gmail.com>
2014-06-27 14:26:23 -0700

    changes to aps_extraction_runer.cpp

--- aps4p5/aps_extraction_runner.cpp
+++ aps4p5/aps_extraction_runner.cpp
@@ -3,15 +3,15 @@
 main(){
 
 aps_extractor test;
-test.set_filename("aps_output/apsWMAP_testrr_guess_output.sav");
+test.set_filename("aps_output/apsWMAP_testBox_guess_output.sav");
 test.set_delta_chi(12.61);
 //test.set_delta_chi(100.0);
 
 test.set_cutoff(30000);
 
-test.write_good_points("apsTestrr_good_points_test10k.sav");
+test.write_good_points("apsTestBox_good_points.sav");
 //test.plot_chimin("aps_chi_min_test.sav");
-test.sample_posterior("apsTestrr_samples_test10k.sav",10000);
+test.sample_posterior("apsTestBox_samples.sav",10000);
 test.show_minpt();
 
 }
