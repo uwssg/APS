1572f25087d6e91cbee8dab0ecf893c19155dc4d
danielsf <scott.f.daniel@gmail.com>
2014-07-10 09:21:46 -0700

    changes to aps_extraction_runner.cpp

--- aps4p5/aps_extraction_runner.cpp
+++ aps4p5/aps_extraction_runner.cpp
@@ -7,9 +7,9 @@ test.set_filename("aps_output/apsWMAP_globalMedian_guess_output.sav");
 test.set_delta_chi(12.61);
 //test.set_delta_chi(100.0);
 
-//test.set_cutoff(10000);
+test.set_cutoff(15000);
 
-test.write_good_points("apsMedian_good_pointsALL.sav");
+test.write_good_points("apsMedian_good_points15k.sav");
 //test.plot_chimin("aps_chi_min_test.sav");
 test.sample_posterior("apsTest2_samples.sav",10000);
 test.show_minpt();
