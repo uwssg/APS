d102c7246ce3c51e7104a45aaf4798081ae437b7
danielsf <scott.f.daniel@gmail.com>
2014-07-18 15:25:54 -0700

    keep track of integrate_boundary

---
+++ aps4p5/integrate_boundary.cpp
@@ -0,0 +1,27 @@
+#include "chisq.h"
+
+main(){
+
+int dim=5,ncenters=3;
+
+ellipses_integrable *chifn;
+
+double v1=1.0,v2=1.0;
+char outname[letters];
+
+int i,j;
+
+for(ncenters=2;ncenters<5;ncenters++){
+    chifn = new ellipses_integrable(dim,ncenters);
+    for(i=0;i<dim;i++){
+        for(j=i+1;j<dim;j++){
+            
+            sprintf(outname,"ellipse_contorls/ellipses_d%d_c%d _%d_%d.sav",dim,ncenters,i,j);
+            
+            chifn->integrate_boundary(0,1,0.95,outname,3.0);
+        }
+    }
+    delete chifn;
+}
+
+}
