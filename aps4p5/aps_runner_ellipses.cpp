7f216b03756f976cc9afca817767d9e4d3b739c0
danielsf <scott.f.daniel@gmail.com>
2014-07-18 23:26:46 -0700

    ellipse runner samples 100k pts

--- aps4p5/aps_runner_ellipses.cpp
+++ aps4p5/aps_runner_ellipses.cpp
@@ -68,7 +68,7 @@ aps_test.set_n_samples(1000);
 double chival,chivaltest,err,maxerr;
 
 i=-1;
-while(aps_test.get_called()<30000){
+while(aps_test.get_called()<100000){
     aps_test.search();    
 }
 
