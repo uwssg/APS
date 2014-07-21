a620da14e80020729f6048c6ce325fc5689eb1f8
danielsf <scott.f.daniel@gmail.com>
2014-07-21 15:06:51 -0700

    starting to document code

--- aps4p5/aps_runner_ellipses.cpp
+++ aps4p5/aps_runner_ellipses.cpp
@@ -11,13 +11,16 @@ main(int iargc, char *argv[]){
 int seed=99;
 int dim,ncenters;
 
-
 seed=atoi(argv[1]);
 dim=atoi(argv[2]);
 ncenters=atoi(argv[3]);
 
 char timingname[letters],outname[letters];
+
+//what is the name of the file where APS will store its timing information
 sprintf(timingname,"aps_output/ellipse/fullAnalysis/ellipse_d%d_c%d_timing.sav",dim,ncenters);
+
+//what is the name of the file where APS will output the points it sampled
 sprintf(outname,"aps_output/ellipse/fullAnalysis/ellipse_d%d_c%d_output.sav",dim,ncenters);
 
 
@@ -27,29 +30,38 @@ if(seed<0){
 
 printf("seed %d\n",seed);
 
-Ran chaos(seed);
-
+//declare the covariogram for APS's Gaussian process
 matern_covariance cv;
 
+//declare the chisquared function APS will be searching
 ellipses_integrable chisq(dim,ncenters);
 
-
+//declare APS
+//the '20' below is the number of nearest neighbors to use when seeding the
+//Gaussian process
+//
+//the '11.0' is the \Delta\chi^2 corresponding to a 95% confidence limit
+//on a 5-dimensional parameter space
 aps aps_test(dim,20,11.0,seed);
 
+//pass chisq to the aps object
 aps_test.assign_chisquared(&chisq);
+
+//pass the covariogram to the aps object
 aps_test.assign_covariogram(&cv);
 
+//how often will APS stop and write its output
 aps_test.set_write_every(1000);
+
+//set the G parameter from equation (4) in the paper
 aps_test.set_grat(1.0);
 
+//set the maximum and minimum values in parameter space
 array_1d<double> max,min;
 max.set_name("driver_max");
 min.set_name("driver_min");
 max.set_dim(dim);
 min.set_dim(dim);
-
-aps_test.set_timingname(timingname);
-aps_test.set_outname(outname);
 
 
 int i,j;
@@ -59,15 +71,23 @@ for(i=0;i<dim;i++){
     max.set(i,100.0);
 }
 
-printf("initializing\n");
-aps_test.initialize(1000,min,max);
-printf("done initializing\n");
 
+aps_test.set_timingname(timingname);
+aps_test.set_outname(outname);
+
+//initialize aps with 1000 random samples
+aps_test.initialize(1000,min,max);
+
+//set N_c from step 2A in the paper
 aps_test.set_n_samples(1000);
 
 double chival,chivaltest,err;
 
 i=-1;
+
+//search parameter space until the
+//chisquared function has been called
+//10000 times
 while(aps_test.get_called()<10000){
     aps_test.search();    
 }
@@ -75,6 +95,8 @@ aps_test.write_pts();
 
 array_1d<double> minpt;
 
+//what is the point in parameter space corresponding to the
+//minimum chisquared
 aps_test.get_minpt(minpt);
 
 printf("chimin %e\n",aps_test.get_chimin());
--- aps4p5/readme.txt
+++ aps4p5/readme.txt
@@ -1,13 +1,28 @@
 21 July 2014
 
-the documentation is now woefully out of date.  I will begin updating it this
-afternoon.
+This is the code implementing the Active Parameter Search algorithm as described
+in REFERENCE.  If you find it at all useful in your research, please cite
+REFERENCE.
 
-If you need to, be aware that aps_runner_ellipses.cpp gives a complete example
-of how to run aps
+This readme file will provide an overview for how the code works.  More specific
+documentation describing the classes and functions involved can be found in the
+.h header files.
 
-I apologize for any inconvenience this may cause.  I just merged a massive
-rewrite of the software and had to focus on editing the manuscript before a
-deadline today.
+Users interesed in a rough-and-ready example can find it in
+aps_runner_ellipses.cpp.  To compile this code simply type
 
--- Scott
+	make ellipse
+
+This will run APS on a multi-modal likelihood function in which each mode is an
+ellipse in parameter space.  Running
+
+	./ellipse 22 5 3
+
+will run APS on a 5-dimensional parameter space with 3 modes (the 22 is the seed
+for the pseuod random number generator used in APS).
+
+	./ellipse 34 6 2
+
+will run APS ona  6-dimensional parameter space with 2 modes.
+
+
