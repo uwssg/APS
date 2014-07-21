ea2e885519bc6cfee1bd964fa89d66feaca99339
danielsf <scott.f.daniel@gmail.com>
2014-07-20 17:33:37 -0700

    fine-tuned aps_extraction_runner.cpp

--- aps4p5/aps_extraction_runner.cpp
+++ aps4p5/aps_extraction_runner.cpp
@@ -7,14 +7,23 @@ test.set_filename("aps_output/WMAP/apsWMAP_unitSphere_NNLearn_output.sav");
 test.set_delta_chi(12.6);
 //test.set_delta_chi(100.0);
 
-test.set_cutoff(30000);
+test.set_cutoff(10000);
 
-test.write_good_points("aps_processed/apsSphere_good_points.sav");
+test.write_good_points("aps_processed/WMAP/frequentist/nnLearn_good_points_10k.sav");
 //test.plot_chimin("aps_chi_min_test.sav");
 //test.sample_posterior("aps_processed/apsSphere_samples.sav",10000);
 
-test.draw_bayesian_bounds("test_dir/apsSphere_lp_bayes_0_2.sav",0,2,0.95);
-test.draw_bayesian_bounds("test_dir/apsSphere_lp_bayes_4_5.sav",4,5,0.95);
+int i,j;
+char outname[letters];
+
+for(i=0;i<6;i++){
+    for(j=i+1;j<6;j++){
+        if(i!=3 && j!=3){
+            sprintf(outname,"aps_processed/WMAP/bayesian/nnLearn_%d_%d_10k_bayes.sav",i,j);
+            test.draw_bayesian_bounds(outname,i,j,0.95);
+        }
+    }
+}
 
 test.show_minpt();
 
