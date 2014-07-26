15ce1b88d189ba4f2ffda366c33ccf38b5ae20d9
danielsf <scott.f.daniel@gmail.com>
2014-07-25 17:31:15 -0700

    documented chisq_aps4p5.h

--- aps4p5/chisq_aps4p5.h
+++ aps4p5/chisq_aps4p5.h
@@ -3,7 +3,9 @@ This file provides the chisquared function wmap_likelihood which allows APS to
 interface with the WMAP 7 year likelihood function.
 
 In order to compile with this option, the user will have to download CAMB and
-the WMAP likelihood code and adjust the Makefile accordingly
+the WMAP likelihood code and adjust the library and include paths in the
+Makefile accordintly
+
 */
 
 #ifndef CHISQ_APS4p5_H
@@ -37,6 +39,12 @@ public:
 #endif
 
 class udder_likelihood : public chisquared{
+
+    /*
+    This is an outdated cartoon likelihood function with two ``real'' minima
+    and one false minimum.  It operates in 6 dimensions.  It sort of looks
+    like a cow's udder when plotting chisquared versus the 0th dimension.
+    */
 
 private:
     mutable int foundn3,foundp3;
