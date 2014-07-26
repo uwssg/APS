ed193785d026d8b58ddbac43ad8e7d2142d4ac84
danielsf <scott.f.daniel@gmail.com>
2014-07-25 17:14:43 -0700

    added aps_runner_wmap.cpp

---
+++ aps4p5/aps_runner_wmap7.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "aps.h"
+#include "chisq_aps4p5.h"
+
+main(int iargc, char *argv[]){
+
+int seed=99;
+int dim;
+
+if(iargc>1){
+    seed=atoi(argv[1]);
+}
+
+if(seed<0){
+    seed=int(time(NULL));
+}
+
+printf("seed %d\n",seed);
+
+Ran chaos(seed);
+
+//matern_covariance cv;
+gaussian_covariance_multiD cv;
+
+wmap_likelihood chisq;
+dim=6;
+
+aps aps_test(dim,20,12.61,seed);
+//12.61 is the 95% CL for 6 dof
+
+aps_test.assign_chisquared(&chisq);
+aps_test.assign_covariogram(&cv);
+
+aps_test.set_write_every(1000);
+aps_test.set_grat(1.0);
+
+aps_test.set_timingname("apsWMAP_smarterBisectionFocus10_guess_timing.sav");
+aps_test.set_outname("apsWMAP_smarterBisectionFocus10_guess_output.sav");
+
+array_1d<double> max,min;
+max.set_name("driver_max");
+min.set_name("driver_min");
+max.set_dim(dim);
+min.set_dim(dim);
+
+min.set(0,0.01);
+max.set(0,0.04);
+
+min.set(1,0.01);
+max.set(1,0.3);
+
+min.set(2,0.4);
+max.set(2,1.0);
+
+min.set(3,0.01);
+max.set(3,0.15);
+
+min.set(4,0.7);
+max.set(4,1.3);
+
+min.set(5,2.0);
+max.set(5,4.0);
+
+int i;
+for(i=0;i<dim;i++){
+    chisq.set_min(i,min.get_data(i));
+    chisq.set_max(i,max.get_data(i));
+}
+
+//aps_test.set_min(min);
+//aps_test.set_max(max);
+aps_test.set_n_samples(250);
+aps_test.initialize(1000,min,max);
+
+array_1d<double> guess;
+guess.set(0,2.191591e-02); 
+guess.set(1,1.134235e-01);
+guess.set(3,1.023216e-02);
+guess.set(4,9.550505e-01);
+guess.set(5,3.077629e+00); 
+guess.set(2,6.889954e-01); 
+
+aps_test.guess(guess);
+
+//aps_test.set_min(min);
+//aps_test.set_max(max);
+//aps_test.resume();
+
+double chival,chivaltest,err,maxerr;
+
+while(aps_test.get_called()<100000){
+    aps_test.search();
+       
+}
+
+aps_test.write_pts();
+
+printf("maxerr %e npts %d\n",maxerr,aps_test.get_n_pts());
+
+}
--- aps4p5/chisq_aps4p5.h
+++ aps4p5/chisq_aps4p5.h
@@ -1,3 +1,11 @@
+/*
+This file provides the chisquared function wmap_likelihood which allows APS to
+interface with the WMAP 7 year likelihood function.
+
+In order to compile with this option, the user will have to download CAMB and
+the WMAP likelihood code and adjust the Makefile accordingly
+*/
+
 #ifndef CHISQ_APS4p5_H
 #define CHISQ_APS4p5_H
 
@@ -7,15 +15,24 @@
 
 #ifdef _WMAP7_
 
-//the two external function definitions below are only needed to
-//interface with CAMB and the WMAP 7 likelihood function
-//as provided in aps_cmb_module.cpp
+/*
+the two external function definitions below are only needed to
+interface with CAMB and the WMAP 7 likelihood function
+as provided in aps_cmb_module.cpp
+*/
 
 extern "C" void \
 camb_wrap_(double*,double*,double*,double*,double*,double*);
 
 extern "C" void wmaplikeness_(double*,double*,double*,double*,double*);
 
+class wmap_likelihood : public chisquared{
+
+public:
+    wmap_likelihood();
+    ~wmap_likelihood();
+    virtual double operator()(array_1d<double>&) const;
+};
 
 #endif
 
@@ -34,15 +51,5 @@ public:
 
 };
 
-#ifdef _WMAP7_
-class wmap_likelihood : public chisquared{
-
-public:
-    wmap_likelihood();
-    ~wmap_likelihood();
-    virtual double operator()(array_1d<double>&) const;
-};
-
-#endif
 
 #endif
