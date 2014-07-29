2108f25b426f545f92f417431d062648a40cddb9
danielsf <scott.f.daniel@gmail.com>
2014-07-29 15:18:26 -0700

    commented aps_runner_wmap7.cpp

--- aps4p5/aps_runner_wmap7.cpp
+++ aps4p5/aps_runner_wmap7.cpp
@@ -7,8 +7,11 @@
 main(int iargc, char *argv[]){
 
 int seed=99;
-int dim;
+int dim,n_init,n_end;
 
+/*
+read in the seed from the command line arguments
+*/
 if(iargc>1){
     seed=atoi(argv[1]);
 }
@@ -17,28 +20,57 @@ if(seed<0){
     seed=int(time(NULL));
 }
 
+if(iargc>2){
+    n_init=atoi(argv[2]);
+    /*
+    set the number of initial samples from the command line arguments
+    */
+}
+else{
+    n_init=1000;
+}
+
+if(iargc>3){
+    n_end=atoi(argv[3]);
+    /*
+    set the total number of calls to chisquared from the command line arguments
+    */
+}
+else{
+    n_end=50000;
+}
+
 printf("seed %d\n",seed);
 
-Ran chaos(seed);
-
-//matern_covariance cv;
+/*declare the covariogram*/
 gaussian_covariance_multiD cv;
 
+/*declare the chisquared function*/
 wmap_likelihood chisq;
 dim=6;
 
+/*declare the aps object:
+dim = dimensionality of parameter space
+20 = number of nearest neighbors used for Gaussian Process
+12.6 = delta chisquared for limit (this is 95% confidence limit for 6 degrees of freedom)
+seed = seed for pseudo random number generator*/
 aps aps_test(dim,20,12.6,seed);
-//12.61 is the 95% CL for 6 dof
 
+/*assign the covariogram and chisquared function*/
 aps_test.assign_chisquared(&chisq);
 aps_test.assign_covariogram(&cv);
 
+/*how often should the code write outputs*/
 aps_test.set_write_every(1000);
+
+/*set the G parameter from equation (4) in the paper*/
 aps_test.set_grat(1.0);
 
-aps_test.set_timingname("output_140718/apsWMAP_unitSphere_GaussLearn_timing.sav");
-aps_test.set_outname("output_140718/apsWMAP_unitSphere_GaussLearn_output.sav");
+/*set the names of the output files*/
+aps_test.set_timingname("outputFiles/apsWMAP_test_timing.sav");
+aps_test.set_outname("timingFiles/apsWMAP_test_output.sav");
 
+/*set the minimum and maximum bounds of parameter space*/
 array_1d<double> max,min;
 max.set_name("driver_max");
 min.set_name("driver_min");
@@ -65,25 +97,35 @@ max.set(5,4.0);
 
 int i;
 for(i=0;i<dim;i++){
+    /*
+    the wmap7 chisquared function allows for minimum and maximum bounds in parameter space
+    outside of which chisquared = 2.0 x 10^30
+    
+    set those here
+    */
     chisq.set_min(i,min.get_data(i));
     chisq.set_max(i,max.get_data(i));
 }
 
-//aps_test.set_min(min);
-//aps_test.set_max(max);
+/*how many candidate points will be proposed in step 2A*/
 aps_test.set_n_samples(250);
 
-aps_test.initialize(1000,min,max);
+/*initialize the aps object with n_init random samples*/
+aps_test.initialize(n_init,min,max);
 
-double chival,chivaltest,err,maxerr;
 
-while(aps_test.get_called()<100000){
-    aps_test.search();
-       
+while(aps_test.get_called()<n_end){
+    /*
+    sample until we have called chisquared the desired number
+    of times
+    */
+
+    aps_test.search();       
 }
 
+/*
+write the final outputs
+*/
 aps_test.write_pts();
-
-printf("maxerr %e npts %d\n",maxerr,aps_test.get_n_pts());
 
 }
