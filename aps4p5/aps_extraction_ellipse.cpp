e54bcd7a2c9f0f4ed241a95f72ac85cc6ee4ac60
danielsf <scott.f.daniel@gmail.com>
2014-08-27 15:38:24 -0700

    added a little bit of documentation to aps_extraction_ellipse.pp

--- aps4p5/aps_extraction_ellipse.cpp
+++ aps4p5/aps_extraction_ellipse.cpp
@@ -1,27 +1,31 @@
 #include "aps_extractor.h"
 
+/*runs aps_extractor on outputs from the ellipses_integrable chisquared function
+
+this can be compiled with
+
+make aps_extract_ellipse
+*/
+
 main(){
 
 aps_extractor test;
 test.set_filename("aps_output/ellipse/fullAnalysis/ellipse_d5_c4_output.sav");
 test.set_delta_chi(11.0);
-//test.set_delta_chi(100.0);
 
 //test.set_cutoff(15000);
 
-//test.write_good_points("aps_processed/ellipse/frequentist/ellipse_d5_c2_good.sav");
 //test.plot_chimin("aps_chi_min_test.sav");
-//test.sample_posterior("apsEllipse_samples.sav",10000);
 
-char bayesname[letters];
+char filename[letters];
 int i,j;
 for(i=0;i<5;i++){
     for(j=i+1;j<5;j++){
-        sprintf(bayesname,"aps_processed/ellipse/bayesianThinned/ellipse_d5_c4_%d_%d_bayes.sav",i,j);
-        test.draw_bayesian_bounds(bayesname,i,j,0.95);
+        sprintf(filename,"aps_processed/ellipse/bayesianThinned/ellipse_d5_c4_%d_%d_bayes.sav",i,j);
+        test.draw_bayesian_bounds(filename,i,j,0.95);
         
-        sprintf(bayesname,"aps_processed/ellipse/frequentistThinned/ellipse_d5_c4_%d_%d_frequentist.sav",i,j);
-        test.write_good_points(bayesname,i,j);
+        sprintf(filename,"aps_processed/ellipse/frequentistThinned/ellipse_d5_c4_%d_%d_frequentist.sav",i,j);
+        test.write_good_points(filename,i,j);
     }
 }
 
